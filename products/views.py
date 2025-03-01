607771028bec0c232e20e8ce91711d16c2970ba7
demes <ninucakott@gmail.com>
2025-03-01 09:25:56 +0400

    davaleba inventory

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from .models import *
 from  .serializers import *
+from rest_framework import viewsets
 
+
+
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
